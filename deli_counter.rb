27f88b8f2855de5672126758cf25c31b4b605fbf
jmcneal89 <jmcneal.writer@gmail.com>
2021-01-18 22:32:22 -0600

    commit tree 6a1685e7dcd87c4958b2f3e01792c43cf5681753

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(katz_deli)
+    katz_deli.nil?
+        puts "The line is currently empty."
+end
+
+def line(other_deli)
+    new_modified_array = other_deli.map.with_index(1) do |name, index|
+        "#{index}. #{name}"
+    end
+    if other_deli.length > 0
+        puts "The line is currently: #{new_modified_array.join(" ")}"
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else
+    puts "Currently serving #{array.first}."
+    array.shift
+    end
+end
